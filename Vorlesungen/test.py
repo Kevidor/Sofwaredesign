805809963299e9da812f30df38c77f380ab6297d
Kevidor <kevintoni@gmx.at>
2024-11-29 10:08:27 +0100

    itertools test

--- Vorlesungen/test.py
+++ Vorlesungen/test.py
@@ -1,1 +1,13 @@
-print("hello world")
+import itertools as itt
+
+max_weight = 15
+weights =  [12, 2, 1, 4, 1]
+values =  [4, 2, 1, 10, 2]
+
+valid_combinations = []
+for r in range(1, len(weights) + 1):  # r gibt die Länge der Kombinationen an
+    for combo in itt.combinations(weights, r):
+        if sum(combo) <= max_weight:
+            valid_combinations.append(combo)
+
+print(valid_combinations)
