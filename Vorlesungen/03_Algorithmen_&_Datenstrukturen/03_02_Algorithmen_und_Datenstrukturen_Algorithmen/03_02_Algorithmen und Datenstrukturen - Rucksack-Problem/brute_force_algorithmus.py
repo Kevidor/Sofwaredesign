dae49a003e1375a93c72cf4dc3c559756642f908
Kevidor <kevintoni@gmx.at>
2024-11-30 12:06:30 +0100

    brute Force Algorithmus für knapsack problem

---
+++ Vorlesungen/03_Algorithmen_&_Datenstrukturen/03_02_Algorithmen_und_Datenstrukturen_Algorithmen/03_02_Algorithmen und Datenstrukturen - Rucksack-Problem/brute_force_algorithmus.py
@@ -0,0 +1,39 @@
+from itertools import combinations
+
+class Objekt():
+    _idx_counter = 0
+    def __init__(self, weight, value) -> None:
+        self.weight = weight
+        self.value = value
+        self.idx = Objekt._idx_counter
+        Objekt._idx_counter += 1
+
+    def __str__(self) -> str:
+        return f"Objekt mit Index: {self.idx}, Weight: {self.weight} und Value: {self.value}"
+    
+    def __repr__(self) -> str:
+        return f"Objekt_{self.idx}(Weight: {self.weight}, Value: {self.value})"
+
+max_weight = 15
+weights =  [12, 2, 1, 4, 1]
+values =  [4, 2, 1, 10, 2]
+
+objekte = [Objekt(wei, val) for wei, val in zip(weights, values)]
+best_value = 0
+best_combo = []
+for i in range(1, len(objekte)):
+    for comb in combinations(objekte, i):
+        test_weight = 0
+        test_value = 0
+        for obj in comb:
+            if test_weight + obj.weight <= max_weight:
+                test_weight += obj.weight
+                test_value += obj.value
+                
+        if test_value > best_value:
+            best_value = test_value
+            best_combo = comb
+
+print(f"Value: {best_value} mit der Combo {best_combo}")
+
+        
