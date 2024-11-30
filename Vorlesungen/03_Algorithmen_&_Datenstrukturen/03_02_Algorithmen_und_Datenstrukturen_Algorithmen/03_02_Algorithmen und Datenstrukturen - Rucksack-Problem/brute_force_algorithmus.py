d751c87668e0ec6e4dfadc10b50efe1cb1a9cbdd
Kevidor <kevintoni@gmx.at>
2024-11-30 12:07:11 +0100

    brute force

--- Vorlesungen/03_Algorithmen_&_Datenstrukturen/03_02_Algorithmen_und_Datenstrukturen_Algorithmen/03_02_Algorithmen und Datenstrukturen - Rucksack-Problem/brute_force_algorithmus.py
+++ Vorlesungen/03_Algorithmen_&_Datenstrukturen/03_02_Algorithmen_und_Datenstrukturen_Algorithmen/03_02_Algorithmen und Datenstrukturen - Rucksack-Problem/brute_force_algorithmus.py
@@ -13,26 +13,30 @@ class Objekt():
     
     def __repr__(self) -> str:
         return f"Objekt_{self.idx}(Weight: {self.weight}, Value: {self.value})"
+    
+def brute_force(objects: list[Objekt]) -> tuple[int, list[Objekt]]:
+    best_value = 0
+    best_combo = []
+    for i in range(1, len(objekte)):
+        for comb in combinations(objekte, i):
+            test_weight = 0
+            test_value = 0
+            for obj in comb:
+                if test_weight + obj.weight <= max_weight:
+                    test_weight += obj.weight
+                    test_value += obj.value
+                    
+            if test_value > best_value:
+                best_value = test_value
+                best_combo = comb
+    return (best_value, best_combo)
 
 max_weight = 15
 weights =  [12, 2, 1, 4, 1]
 values =  [4, 2, 1, 10, 2]
 
 objekte = [Objekt(wei, val) for wei, val in zip(weights, values)]
-best_value = 0
-best_combo = []
-for i in range(1, len(objekte)):
-    for comb in combinations(objekte, i):
-        test_weight = 0
-        test_value = 0
-        for obj in comb:
-            if test_weight + obj.weight <= max_weight:
-                test_weight += obj.weight
-                test_value += obj.value
-                
-        if test_value > best_value:
-            best_value = test_value
-            best_combo = comb
+best_value, best_combo = brute_force(objekte)
 
 print(f"Value: {best_value} mit der Combo {best_combo}")
 
