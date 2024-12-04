7cbde92c69a1508ef5f12eba1d069786e0340f95
Kevidor <kevintoni@gmx.at>
2024-12-04 21:07:35 +0100

    majority_vote aufgabe

--- Vorlesungen/03_Algorithmen_&_Datenstrukturen/03_04_Algorithmen_und_Datenstrukturen_Zeit-Platzkomplexität/test.py
+++ Vorlesungen/03_Algorithmen_&_Datenstrukturen/03_04_Algorithmen_und_Datenstrukturen_Zeit-Platzkomplexität/test.py
@@ -1,12 +1,29 @@
+from functools import wraps
 import time
+import random
+from collections import Counter
 
-start_time = time.perf_counter()
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'@timeit: {func.__name__} took {(total_time*1e3):.4f} milliseconds')
+        #return result, total_time
+        return result
+    return timeit_wrapper
 
-sum = 0
-for i in range(0, 10000):
-    sum += i*i
+@timeit
+def find_majority_element_efficient(a_list):
+    counter = Counter(a_list)
+    value, count = counter.most_common(1)[0]
+    if count > len(a_list)/2: return value
+    return False
 
-end_time = time.perf_counter()
+my_list = [2, 8, 7, 4, 1, 5, 2, 3, 1, 2, 2]
+my_list_2 = [2, 8, 7, 2, 2, 5, 2, 3, 1, 2, 2]
 
-delta_time = end_time - start_time
-print(f"delta_time = {delta_time * 1e3}")
+print(find_majority_element_efficient(my_list))
+print(find_majority_element_efficient(my_list_2))
